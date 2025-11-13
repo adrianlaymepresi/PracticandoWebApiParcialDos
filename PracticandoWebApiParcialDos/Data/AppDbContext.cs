83af814e33a80e90f784c00266d172f144bd47a9
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-11-13 16:56:33 -0400

    AdrianLayme: Creacion del Context

---
+++ PracticandoWebApiParcialDos/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticandoWebApiParcialDos.Models;
+
+namespace PracticandoWebApiParcialDos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Rol> Roles => Set<Rol>();
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+        public DbSet<Cliente> Clientes => Set<Cliente>();
+        public DbSet<Producto> Productos => Set<Producto>();
+        public DbSet<Pedido> Pedidos => Set<Pedido>();
+        public DbSet<DetallePedido> DetallePedidos => Set<DetallePedido>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
